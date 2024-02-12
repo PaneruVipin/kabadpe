64788399f7a576fb9cf629ae4bf31dbfe06c14d1
PaneruVipin <paneruvipinbusiness@gmail.com>
2024-02-12 04:28:11 -0800

    admin indivisule subs

--- src/AdminComponents.jsx/ForIndividuals.jsx
+++ src/AdminComponents.jsx/ForIndividuals.jsx
@@ -2,30 +2,31 @@ import React, { useState } from "react";
 import IndPlanForm from "./IndPlanForm";
 import MonthlyPlan from "./MonthlyPlan";
 import QuaterPlan from "./QuaterPlan";
+import { useQuery } from "@tanstack/react-query";
+import { adminWorkerSubsFetch } from "../apis/admins/subscription";
 
-const ForIndividuals = ({onSwitch , onactive , onSwitchPrev}) => {
-    const [indPlanForm , setIndPlanForm] = useState(null);
-    const [monthlyPlan, setMonthlyPlan] = useState(null);
-    const [quaterPlan, setQuaterPlan] = useState(null);
-
-
-
+const ForIndividuals = ({ onSwitch, onactive, onSwitchPrev }) => {
+  const [indPlanForm, setIndPlanForm] = useState(null);
+  const [formType, setFormType] = useState("weekly");
+  const [formval, setFormVal] = useState(null);
+  const { data: plansData, refetch } = useQuery({
+    queryKey: ["adminfetcWorkerSubs"],
+    queryFn: () => adminWorkerSubsFetch(),
+  });
+  console.log("plansData plansDataplansData", plansData);
   return (
     <>
       <section className="add-work-comn-comp">
         <div className="add-work-btn-flex-bx">
-          <h6 className="banktext mb-0">Subscriptions Plans</h6>
-
-        
-
+          <h6 className="banktext mb-0">Subscriptions Plans </h6>
           <div className="for-french-indi-flex-btn">
             <button
               className={
                 onactive === "addsubscription"
                   ? "switch-btn switchactive"
                   : "switch-btn "
-              } onClick={onSwitchPrev}
-              
+              }
+              onClick={onSwitchPrev}
             >
               For Frenchies
             </button>
@@ -41,106 +42,196 @@ const ForIndividuals = ({onSwitch , onactive , onSwitchPrev}) => {
               For Individuals
             </button>
           </div>
-
-         
+          <button
+            // onClick={() => {
+            //   setInitialUpdateValues();
+            //   setSubsPlanBx(true);
+            // }}
+            className="add-work-btn-comn add-work-btn-comn2 addnew-work-btn"
+          >
+            Edit Max Individual Service Arias
+          </button>
         </div>
 
         <div className="all-user-table add-wrk-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>SNo.</th>
-                        <th>Plan Name</th>
-                        <th>Fixed Price</th>
-                        <th>Comission Price</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <span>1</span>
-                        </td>
-                        <td>
-                            <span>Plan A Name</span>
-                        </td>
-                        <td>
-                            <span>₹300 <span className="mini-text">/week</span> </span>
-                        </td>
-                        <td>
-                        <span> ₹2 <span className="plus-sign">+</span> 2.5%<span className="mini-text">(weekly)</span></span>
-                        </td>
-                        <td>
-                            <div className="edit-remv-btns">
-                                <button onClick={() => setIndPlanForm(true)}>Edit</button>
-                            </div>
-                        </td>
-                    </tr>
+          <table>
+            <thead>
+              <tr>
+                <th>SNo.</th>
+                <th>Plan Name</th>
+                <th>Plan Type</th>
+                <th>Fixed Price</th>
+                <th>Comission</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!plansData?.error ? (
+                <>
+                  <tr>
+                    <td>
+                      <span>1</span>
+                    </td>
+                    <td>
+                      <span>{plansData?.A?.planeName}</span>
+                    </td>
+                    <td>
+                      <span>Weekly</span>
+                    </td>
+                    <td>
+                      {plansData?.A ? (
+                        <span>
+                          ₹ {plansData?.A?.fixedAmount}{" "}
+                          <span className="mini-text"></span>{" "}
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      {plansData?.A ? (
+                        <span>
+                          {" "}
+                          ₹{plansData?.A?.comissionBaseAmount}
+                          <span className="plus-sign">+</span>
+                          {plansData?.A?.comissionRate}%
+                          <span className="mini-text"></span>
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      <div className="edit-remv-btns">
+                        <button
+                          onClick={() => {
+                            setIndPlanForm(true);
+                            setFormType("weekly");
+                            setFormVal({
+                              comissionRate: plansData?.A?.comissionRate,
+                              comissionBaseAmount:
+                                plansData?.A?.comissionBaseAmount,
+                              fixedAmount: plansData?.A?.fixedAmount,
+                              planeName: plansData?.A?.planeName,
+                            });
+                          }}
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
 
-                    <tr>
-                        <td>
-                            <span>2</span>
-                        </td>
-                        <td>
-                            <span>Plan B Name</span>
-                        </td>
-                        <td>
-                            <span>₹300<span className="mini-text">/monthly</span></span>
-                        </td>
-                        <td>
-                        <span> ₹5 <span className="plus-sign">+</span> 2.5%<span className="mini-text">(monthly)</span></span>
-                        </td>
-                        <td>
-                            <div className="edit-remv-btns">
-                                <button onClick={() => setMonthlyPlan(true)}>Edit</button>
-                            </div>
-                        </td>
-                    </tr>
+                  <tr>
+                    <td>
+                      <span>2</span>
+                    </td>
+                    <td>
+                      <span>{plansData?.B?.planeName}</span>
+                    </td>
+                    <td>
+                      <span>Monthly</span>
+                    </td>
+                    <td>
+                      {plansData?.B ? (
+                        <span>
+                          ₹ {plansData?.B?.fixedAmount}{" "}
+                          <span className="mini-text"></span>{" "}
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      {plansData?.B ? (
+                        <span>
+                          {" "}
+                          ₹{plansData?.B?.comissionBaseAmount}
+                          <span className="plus-sign">+</span>
+                          {plansData?.B?.comissionRate}%
+                          <span className="mini-text"></span>
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      <div className="edit-remv-btns">
+                        <button
+                          onClick={() => {
+                            setIndPlanForm(true);
+                            setFormType("monthly");
+                            setFormVal({
+                              comissionRate: plansData?.B?.comissionRate,
+                              comissionBaseAmount:
+                                plansData?.B?.comissionBaseAmount,
+                              fixedAmount: plansData?.B?.fixedAmount,
+                              planeName: plansData?.B?.planeName,
+                            });
+                          }}
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
 
-
-                    <tr>
-                        <td>
-                            <span>3</span>
-                        </td>
-                        <td>
-                            <span>Plan C Name</span>
-                        </td>
-                        <td>
-                            <span>₹300<span className="mini-text">/quaterly</span></span>
-                        </td>
-                        <td>
-                            <span> ₹7 <span className="plus-sign">+</span> 2.5%<span className="mini-text">(quaterly)</span></span>
-                        </td>
-                        <td>
-                            <div className="edit-remv-btns">
-                                <button onClick={() => setQuaterPlan(true)}>Edit</button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                  <tr>
+                    <td>
+                      <span>3</span>
+                    </td>
+                    <td>
+                      <span>{plansData?.C?.planeName}</span>
+                    </td>
+                    <td>
+                      <span>Quaterly</span>
+                    </td>
+                    <td>
+                      {plansData?.C ? (
+                        <span>
+                          ₹ {plansData?.C?.fixedAmount}{" "}
+                          <span className="mini-text"></span>{" "}
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      {plansData?.C ? (
+                        <span>
+                          {" "}
+                          ₹{plansData?.C?.comissionBaseAmount}
+                          <span className="plus-sign">+</span>
+                          {plansData?.C?.comissionRate}%
+                          <span className="mini-text"></span>
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      <div className="edit-remv-btns">
+                        <button
+                          onClick={() => {
+                            setIndPlanForm(true);
+                            setFormType("quaterly");
+                            setFormVal({
+                              comissionRate: plansData?.C?.comissionRate,
+                              comissionBaseAmount:
+                                plansData?.C?.comissionBaseAmount,
+                              fixedAmount: plansData?.C?.fixedAmount,
+                              planeName: plansData?.C?.planeName,
+                            });
+                          }}
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                </>
+              ) : null}
+            </tbody>
+          </table>
         </div>
-       
       </section>
 
-      { indPlanForm ? (
-
-      <IndPlanForm onClickClose={() => setIndPlanForm(null)} /> 
-      ) : null 
-       }
-
-{ monthlyPlan ? (
-
-<MonthlyPlan onClickCloseMonthly={() => setMonthlyPlan(null)} /> 
-) : null 
- }
-
-{ quaterPlan ? (
-
-<QuaterPlan onClickCloseQuaterly={() => setQuaterPlan(null)} /> 
-) : null 
- }
-
+      {indPlanForm ? (
+        <IndPlanForm
+          formType={formType}
+          formval={formval}
+          refetch={refetch}
+          onClickClose={() => setIndPlanForm(null)}
+        />
+      ) : null}
     </>
   );
 };
--- src/AdminComponents.jsx/IndPlanForm.jsx
+++ src/AdminComponents.jsx/IndPlanForm.jsx
@@ -1,248 +1,161 @@
-import React from 'react'
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import { adminWorkerSubsInsert } from "../apis/admins/subscription";
 
-const IndPlanForm = ({onClickClose}) => {
+const IndPlanForm = ({
+  formType = "weekly",
+  onClickClose,
+  formval,
+  refetch,
+}) => {
+  const [otherErrors, setOtherErrors] = useState({});
+  const planTypes = {
+    weekly: "Plan A (Weekly)",
+    monthly: "Plan B (Monthly)",
+    quaterly: "Plan C (Quaterly)",
+  };
+  const planType = planTypes[formType];
+  const initialValues = formval || {
+    comissionRate: "",
+    comissionBaseAmount: "",
+    fixedAmount: "",
+    planeName: "",
+  };
+  const handleSubmit = async (data) => {
+    setOtherErrors({});
+    const res = await adminWorkerSubsInsert({ ...data, planType: formType });
+    if (!res?.error) {
+      refetch();
+      onClickClose();
+      return;
+    }
+    setOtherErrors({ insert: res?.message });
+  };
   return (
     <>
+      <section className="indi-plan-form-comp" onClick={onClickClose}>
+        <div
+          className="ind-plan-form-main"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <h3>Plan For Individual</h3>
 
-    <section className="indi-plan-form-comp" onClick={onClickClose}>
-
-        <div className="ind-plan-form-main" onClick={(e) => e.stopPropagation()}>
-            <h3>Plan For Individual</h3>
-
-            <form action="#">
-
-                
-                <div className="ind-plan-form-bx">
-
-                <div className="admin-login-fild">
-                        <label htmlFor="planname">Plan  A (Weekly) </label>
-                        <div className="admin-login-input">
-                          <input
-                            type="text"
-                            name="planeName"
-                            id="planname"
-                            placeholder="Enter  Plan Name"
-                            autoComplete="off"
-                       
-                          />
-                        </div>
-                      
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            // validationSchema={object().shape({
+            //   otp: number().required("Valid OTP Requird"),
+            // })}
+          >
+            {({
+              handleBlur,
+              handleChange,
+              values,
+              errors,
+              touched,
+              ...rest
+            }) => {
+              return (
+                <Form>
+                  <div className="ind-plan-form-bx">
+                    <div className="admin-login-fild">
+                      <label htmlFor="planname">{planType}</label>
+                      <div className="admin-login-input">
+                        <input
+                          type="text"
+                          name="planeName"
+                          id="planname"
+                          placeholder="Enter  Plan Name"
+                          autoComplete="off"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values?.planeName}
+                        />
                       </div>
+                      {/* {touched.otp && errors.otp ? (
+                        <div style={{ color: "red" }}>{errors.otp}</div>
+                      ) : null} */}
+                    </div>
 
-                      <div className="admin-login-fild">
-                        <label htmlFor="planname">Fixed</label>
-                        <div className="input-parent-bx">
+                    <div className="admin-login-fild">
+                      <label htmlFor="planname">Fixed</label>
+                      <div className="input-parent-bx">
                         <div className="admin-login-input">
                           <input
-                            type="text"
-                            name="planeName"
+                            type="number"
+                            name="fixedAmount"
                             id="planname"
                             placeholder="Enter Fixed Price"
                             autoComplete="off"
-                       
+                            onChange={handleChange}
+                            onWheel={(e) => e.currentTarget.blur()}
+                            onBlur={handleBlur}
+                            value={values?.fixedAmount}
                           />
                         </div>
                         <span>₹/Lead</span>
-                        </div>
                       </div>
+                    </div>
 
-                      <div className="admin-login-fild">
-                        <label htmlFor="planname">Comission</label>
-                        <div className="input-parent-main-grid">
+                    <div className="admin-login-fild">
+                      <label htmlFor="planname">Comission</label>
+                      <div className="input-parent-main-grid">
                         <div className="input-parent-bx input-parent-bx2">
-                        <div className="admin-login-input">
-                          <input
-                            type="text"
-                            name="planeName"
-                            id="planname"
-                            placeholder="Enter ₹"
-                            autoComplete="off"
-                       
-                          />
-                        </div>
-                        <span>₹</span>
+                          <div className="admin-login-input">
+                            <input
+                              type="number"
+                              name="comissionBaseAmount"
+                              id="planname"
+                              placeholder="Enter ₹"
+                              autoComplete="off"
+                              onChange={handleChange}
+                              onWheel={(e) => e.currentTarget.blur()}
+                              onBlur={handleBlur}
+                              value={values?.comissionBaseAmount}
+                            />
+                          </div>
+                          <span>₹</span>
                         </div>
 
                         <div className="plus">
-                        <i class="fa-solid fa-plus"></i>
+                          <i class="fa-solid fa-plus"></i>
                         </div>
 
                         <div className="input-parent-bx input-parent-bx2">
-                        <div className="admin-login-input">
-                          <input
-                            type="text"
-                            name="planeName"
-                            id="planname"
-                            placeholder="Enter %"
-                            autoComplete="off"
-                       
-                          />
+                          <div className="admin-login-input">
+                            <input
+                              type="number"
+                              name="comissionRate"
+                              id="planname"
+                              placeholder="Enter %"
+                              autoComplete="off"
+                              onChange={handleChange}
+                              onWheel={(e) => e.currentTarget.blur()}
+                              onBlur={handleBlur}
+                              value={values?.comissionRate}
+                            />
+                          </div>
+                          <span>%</span>
                         </div>
-                        <span>%</span>
-                        </div>
                       </div>
-                      </div>
-                      </div>
-
-{/* 
-                      <div className="ind-plan-form-bx">
-
-<div className="admin-login-fild">
-        <label htmlFor="planname">Plan B</label>
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter  Plan  Name"
-            autoComplete="off"
-       
-          />
+                    </div>
+                  </div>
+                  {otherErrors ? (
+                    <div style={{ color: "red", textAlign: "center" }}>
+                      {otherErrors?.insert}
+                    </div>
+                  ) : null}
+                  <button type="submit" className="add-work-area-btn">
+                    Done
+                  </button>
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
-      
-      </div>
-
-      <div className="admin-login-fild">
-        <label htmlFor="planname">Fixed</label>
-        <div className="input-parent-bx">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter Fixed Price"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>₹/Lead</span>
-        </div>
-      </div>
-
-      <div className="admin-login-fild">
-        <label htmlFor="planname">Comission</label>
-        <div className="input-parent-main-grid">
-        <div className="input-parent-bx input-parent-bx2">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter ₹"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>₹</span>
-        </div>
-
-        <div className="plus">
-        <i class="fa-solid fa-plus"></i>
-        </div>
-
-        <div className="input-parent-bx input-parent-bx2">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter %"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>%</span>
-        </div>
-      </div>
-      </div>
-      </div>
-
-
-
-
-      <div className="ind-plan-form-bx">
-
-<div className="admin-login-fild">
-        <label htmlFor="planname">Plan C</label>
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter  Plan  Name"
-            autoComplete="off"
-       
-          />
-        </div>
-      
-      </div>
-
-      <div className="admin-login-fild">
-        <label htmlFor="planname">Fixed</label>
-        <div className="input-parent-bx">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter Fixed Price"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>₹/Lead</span>
-        </div>
-      </div>
-
-      <div className="admin-login-fild">
-        <label htmlFor="planname">Comission</label>
-        <div className="input-parent-main-grid">
-        <div className="input-parent-bx input-parent-bx2">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter ₹"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>₹</span>
-        </div>
-
-        <div className="plus">
-        <i class="fa-solid fa-plus"></i>
-        </div>
-
-        <div className="input-parent-bx input-parent-bx2">
-        <div className="admin-login-input">
-          <input
-            type="text"
-            name="planeName"
-            id="planname"
-            placeholder="Enter %"
-            autoComplete="off"
-       
-          />
-        </div>
-        <span>%</span>
-        </div>
-      </div>
-      </div>
-      </div>
-                     */}
-                <button  className="add-work-area-btn">
-                     Done
-                    </button>
-            </form>
-            
-        </div>
-        
-    </section>
-      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default IndPlanForm
+export default IndPlanForm;

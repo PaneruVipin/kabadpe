15f728608f0cfb335a2e23a79871e85c1ff376fe
Faiz Alam <fz007660@gmail.com>
2024-03-05 10:27:55 +0530

    AdsChanged

--- src/HomeComponent/AdsBanner.jsx
+++ src/HomeComponent/AdsBanner.jsx
@@ -7,11 +7,11 @@ const AdsBanner = () => {
     <div className="ads-banner-bx">
         <div className="comon-container-2 ">
         <div className="ads-info-bx">
-                <p>Repair Service</p>
-                <h4>We're an Apple <br/>
-Authorised Service Provider</h4>
+                <p>Eco Friendly</p>
+               <h4> Earth Friendly Skin <br/> And Home Care Products 
+                 </h4>
         <button className="learm-mre-btn">
-            Learn More
+            Shop Now
         </button>
         </div>
         </div>

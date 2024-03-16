6be004d0015c42104f0ada962389c5b79f5f2242
Kunal Verma <Kunalverma465@gmail.com>
2024-03-16 11:23:35 +0530

    wallet update by admin

--- src/HomeComponent/AdsBanner.jsx
+++ src/HomeComponent/AdsBanner.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const AdsBanner = () => {
   return (
     <>
-
-    <div className="ads-banner-bx">
+      <div className="ads-banner-bx">
         <div className="comon-container-2 ">
-        <div className="ads-info-bx">
-                <p>Shop Green</p>
-               <h4> Earth Friendly Skin <br/> And Home Care Products 
-                 </h4>
-        <button className="learm-mre-btn">
-            Shop Now
-        </button>
+          <div className="ads-info-bx">
+            <p>Shop Green</p>
+            <h4>
+              {" "}
+              Earth Friendly Skin <br /> And Home Care Products
+            </h4>
+            <button className="learm-mre-btn">Shop Now</button>
+          </div>
         </div>
-        </div>
-    </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdsBanner
+export default AdsBanner;

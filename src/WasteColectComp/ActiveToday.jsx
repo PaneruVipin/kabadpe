c0e304f039f1afac29ebaacb8f4913941879f2bc
PaneruVipin <paneruvipinbusiness@gmail.com>
2024-04-15 03:21:46 -0700

    dynamic slots

--- src/WasteColectComp/ActiveToday.jsx
+++ src/WasteColectComp/ActiveToday.jsx
@@ -5,12 +5,13 @@ import {
 } from "../apis/worker/availability";
 import { useQuery } from "@tanstack/react-query";
 
-const ActiveToday = ({ onclickClose, todayTrue }) => {
+const ActiveToday = ({ onclickClose, todayTrue, refetch: refetchStatus }) => {
   const handleMarkActiveToday = async () => {
     const res = await workerMarkActiveToday();
     if (!res?.error) {
       onclickClose();
-      refetch()
+      refetch();
+      refetchStatus();
     }
   };
   const { data: todayAvail, refetch } = useQuery({
@@ -27,13 +28,9 @@ const ActiveToday = ({ onclickClose, todayTrue }) => {
               : "Mark You Are Working Today."}
           </h6>
 
-        
-            <button
-              onClick={handleMarkActiveToday}
-              className="popup-action-btn"
-            >
-              Confirm
-            </button>
+          <button onClick={handleMarkActiveToday} className="popup-action-btn">
+            Confirm
+          </button>
 
           <div onClick={onclickClose} className="close-btn">
             <i class="fa-solid fa-xmark"></i>

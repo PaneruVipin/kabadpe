4234605dc5b73cd9a58fcbf4db196c4710155566
Faiz Alam <fz007660@gmail.com>
2024-04-18 15:27:33 +0530

    Done

---
+++ src/Components/AboutComp.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+
+const AboutComp = () => {
+  return (
+    <>
+      <section className="about-grid-comp about-grid-comp2">
+        <div className="comon-container-2">
+          <div className="about-grid-bx">
+            <div className="right-about-info-bx">
+              <span>ABOUT US</span>
+              <h3>KABADPE: WHERE WASTE TO WEALTH BECOME REALITY</h3>
+
+              <h6>
+                Hi our <span>Sustainability Champion(s), </span>{" "}
+              </h6>
+
+              <p>
+                Welcome to KabadPe, where recycling meets rewards in the most
+                awesome way possible. We're not just another waste management
+                company – we're your partners in turning your trash into cold,
+                hard cash!
+              </p>
+            </div>
+
+            <div className="left-about-grid-img right-about-grid-img">
+              <img src="/images/customImg/waste-img-2.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="about-list-comp">
+        <div className="comon-container-2">
+          <div className="about-list-bx">
+            <div className="left-a-img">
+              <img src="/images/customImg/bottle-2.png" alt="" />
+              <div className="arrow-right arrow-left  mt-5">
+                <img src="/images/customImg/arrows.png" alt="" />
+              </div>
+            </div>
+
+            <div className="right-a-info">
+              <h6>THIS IS A ORDINARY PLASTIC BOTTLE, WE WILL CALL IT ASUR</h6>
+            </div>
+            <div className="arrow-down-img">
+              <img src="/images/customImg/arrows.png" alt="" />
+            </div>
+          </div>
+
+          <div className="about-list-bx">
+            <div className=" right-a-img">
+              <img src="/images/customImg/global.gif" alt="" />
+              <div className="arrow-right  arrow-right-right mt-5">
+                <img src="/images/customImg/arrows.png" alt="" />
+              </div>
+            </div>
+
+            <div className="right-a-info">
+              <h6>
+                ASUR AND HIS FRIENDS ENTERS INTO EATHS ENVIROMENT AND RAISE
+                TEMPERATE CAPTURED AS CLIMATE STRIPES
+              </h6>
+            </div>
+            <div className="arrow-down-img">
+              <img src="/images/customImg/arrows.png" alt="" />
+            </div>
+          </div>
+
+          <div className="about-list-bx">
+            <div className="left-a-img">
+              <img src="/images/customImg/tem-img.png" alt="" />
+              <div className="arrow-right arrow-left">
+                <img src="/images/customImg/arrows.png" alt="" />
+              </div>
+            </div>
+
+            <div className="right-a-info">
+              <h6>
+                CLIMATE STRIPES CAN BE CALLED AS THERMOMETER OF EARTH TO CHECK
+                ITS TEMPERATURE. MORE THE RED BARS MORE THE EARTH IS SICK.
+              </h6>
+            </div>
+            <div className="arrow-down-img">
+              <img src="/images/customImg/arrows.png" alt="" />
+            </div>
+          </div>
+
+          <div className="about-list-bx">
+            <div className=" right-a-img">
+              <img src="/images/customImg/zero-waste-socity.jpg" alt="" />
+              <div className="arrow-right arrow-right-right ">
+                <img src="/images/customImg/arrows.png" alt="" />
+              </div>
+            </div>
+
+            <div className="right-a-info">
+              <h6>
+                THIS WILL LEAD TO DISEASES, MORE ELECTRICITY BILLS, AND NUMEROUS
+                CLIMATE IMPACTS.
+              </h6>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="backtoblue-comp">
+        <div className="comon-container-2">
+          <div className="about-grid-bx about-grid-bx2">
+            <div className="right-about-info-bx left-about-info-bx">
+              <h3>OUR JOURNEY: FUN, ECO-FRIENDLY, AND REWARDING!</h3>
+              <p>
+                After years into working towards environment for different
+                organizations, Gayathri Nair in 2023, started her own venture
+                ClimStripe Shift (CSS) Pvt. Ltd., to shift climate stripes back
+                to blue. CSS comprises of three companies Kabadpe for waste
+                management, The Green Saman Shop for your shopping needs but we
+                removed plastic from your products and provided you alternative,
+                and finally ClimConnect so you can share your climate stories
+                with other eco champions and raise funds for such activities.
+              </p>
+            </div>
+
+            <div className="left-about-grid-img right-about-grid-img">
+              <img src="/images/customImg/clim-log.png" alt="" />
+            </div>
+          </div>
+
+          <h4>
+            #Backto<span>Blue</span>{" "}
+          </h4>
+
+          <div className="backtoblue-logos-flex-bx">
+            <div className="logs-bx">
+              <img src="/images/customImg/logo.png" alt="" />
+            </div>
+
+            <div className="logs-bx">
+              <img src="/images/customImg/tgss-logo-img.png" alt="" />
+            </div>
+
+            <div className="logs-bx">
+              <img src="/images/customImg/logo.png" alt="" />
+            </div>
+          </div>
+
+          <div className="about-grid-bx about-grid-bx2">
+            <div className="right-about-info-bx left-about-info-bx">
+              <h3>TRASH TALK: HOW IT WORKS</h3>
+              <p>
+                So, here's the scoop – every time you recycle with KabadPe,
+                you're not just helping the planet (although that's a big win!).
+                You're also earning wallet money for every item you recycle.
+                From plastic bottles to old gadgets, your trash becomes your
+                treasure!
+              </p>
+            </div>
+
+            <div className="left-about-grid-img right-about-grid-img">
+              <img src="/images/customImg/waste-img-4.jpg" alt="" />
+            </div>
+          </div>
+
+          <div className="about-grid-bx about-grid-bx2 about-grid-bx3 mt-5">
+            <div className="right-about-info-bx left-about-info-bx">
+              <h3>LET'S MAKE RECYCLING RAD TOGETHER!</h3>
+              <p>
+                So, what are you waiting for? Dive into the fun and rewarding
+                world of KabadPe today. Turn your trash into cash, join our
+                green team, and let's show the world that recycling can be epic,
+                enjoyable, and endlessly rewarding!
+              </p>
+            </div>
+          </div>
+
+            <button className="recycle-tod-btn">
+                Recycle Today
+            </button>
+          
+        </div>
+
+     
+        
+      </section>
+    </>
+  );
+};
+
+export default AboutComp;

4a83dadce334b285275f847f171a218d2fc484ef
Faiz Alam <fz007660@gmail.com>
2024-04-22 17:57:10 +0530

    climconnect

---
+++ src/ClimconnectComp/Climconectfollowing.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+
+const Climconectfollowing = () => {
+  return (
+    <>
+
+
+        <div className="clim-conect-folower-flex">
+
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/team-3.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>Faizu011</span>
+                    <h6>Faiz_Alam</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Follow
+                </button>
+            </div>
+
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/team-2.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>theahmed08</span>
+                    <h6>Ahmed Sheikh</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Unfollow
+                </button>
+            </div>
+
+
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/team-4.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>aazibsalim </span>
+                    <h6>Aazi₿ $alim</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Follow
+                </button>
+            </div>
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/c-1.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>mohd.shehzad.ahmed</span>
+                    <h6>
+KHUSH.DIL</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Follow
+                </button>
+            </div>
+
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/c-2.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>farukh_9718</span>
+                    <h6>Farukh Ansari</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Unfollow
+                </button>
+            </div>
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/c-3.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>adnanfahad80</span>
+                    <h6>Adnan fahad</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Unfollow
+                </button>
+            </div>
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/c-4.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>mdmonismansoori</span>
+                    <h6>Md Monis Mansoori</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Follow
+                </button>
+            </div>
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/team-3.jpg" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>its_md_mamun_01</span>
+                    <h6>Md Mamun Rashid</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Follow
+                </button>
+            </div>
+            <div className="climconect-user-folow-bx">
+                <div className="folower-left-bx">
+                <div className="user-folow-img">
+                    <img src="/images/customImg/user-prf-img.webp" alt="" />
+                </div>
+                <div className="user-folow-info">
+                    <span>mddauodking</span>
+                    <h6>
+Md Daoud</h6>
+                </div>
+                </div>
+
+                <button className="folow-btn">
+                        Unfollow
+                </button>
+            </div>
+
+            
+        </div>
+        
+      
+    </>
+  )
+}
+
+export default Climconectfollowing

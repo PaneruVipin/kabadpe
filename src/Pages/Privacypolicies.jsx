379674d3ffdb479f3e8282c368aac1ecdfcd0402
PaneruVipin <paneruvipinbusiness@gmail.com>
2024-06-27 04:58:24 -0700

    miner

--- src/Pages/Privacypolicies.jsx
+++ src/Pages/Privacypolicies.jsx
@@ -1,59 +1,60 @@
-import React from 'react'
-import ServBanner from '../ServComps/ServBanner';
-import MainFooter from '../HomeComponent/MainFooter';
+import React from "react";
+import ServBanner from "../ServComps/ServBanner";
+import MainFooter from "../HomeComponent/MainFooter";
 
 const Privacypolicies = () => {
-        const data = {
-          title: "Privacy Policies",
-          link: "Privacy policies",
-        };
+  const data = {
+    title: "Privacy Policies",
+    link: "Privacy policies",
+  };
   return (
     <>
-       <ServBanner data={data} />
-    
+      <ServBanner data={data} />
+
       <section className="serv-comp-two">
         <div className="comon-container-2">
-
           <div className="waste-sip-list">
             <li>
               <p>
-              We are committed to safeguarding the privacy of [our website visitors and
-service users].
+                We are committed to safeguarding the privacy of [our website
+                visitors and service users].
               </p>
             </li>
             <li>
               <p>
-              This policy applies where we are acting as a data controller with respect to
-the personal data of [our website visitors and service users]; in other words,
-where we determine the purposes and means of the processing of that personal
-data.
+                This policy applies where we are acting as a data controller
+                with respect to the personal data of [our website visitors and
+                service users]; in other words, where we determine the purposes
+                and means of the processing of that personal data.
               </p>
-
-        
-              
             </li>
 
             <li>
-                <p>We use cookies on our website. Insofar as those cookies are not strictly
-necessary for the provision of [our website and services], we will ask you to
-consent to our use of cookies when you first visit our website.</p>
+              <p>
+                We use cookies on our website. Insofar as those cookies are not
+                strictly necessary for the provision of [our website and
+                services], we will ask you to consent to our use of cookies when
+                you first visit our website.
+              </p>
             </li>
 
             <li>
-                <p>Our website incorporates privacy controls which affect how we will process
-your personal data. By using the privacy controls, you can [specify whether you
-would like to receive direct marketing communications and limit the publication
-of your information]. You can access the privacy controls via [URL].</p>
+              <p>
+                Our website incorporates privacy controls which affect how we
+                will process your personal data. By using the privacy controls,
+                you can [specify whether you would like to receive direct
+                marketing communications and limit the publication of your
+                information]. You can access the privacy controls via [URL].
+              </p>
             </li>
 
             <li>
-                <p>
-                In this policy, &quot;we&quot;, &quot;us&quot; and &quot;our&quot; refer to [data controller name]. [ For more
-information about us, see Section 13.]
-                </p>
+              <p>
+                In this policy, &quot;we&quot;, &quot;us&quot; and
+                &quot;our&quot; refer to [data controller name]. [ For more
+                information about us, see Section 13.]
+              </p>
             </li>
-
-          
           </div>
         </div>
       </section>
@@ -61,152 +62,164 @@ information about us, see Section 13.]
       <section className="serv-comp-one">
         <div className="comon-container-2">
           <div className="full-text ">
-            <p className='mb-4'>
-            CLIMSTRIPE SHIFT PRIVATE LIMITED operates waste management under the
-brand KabadPe and the Website Name https://www.kabadpe.com, which
-provides the SERVICE.
+            <p className="mb-4">
+              CLIMSTRIPE SHIFT PRIVATE LIMITED operates waste management under
+              the brand KabadPe and the Website Name https://www.kabadpe.com,
+              which provides the SERVICE.
             </p>
-            <p className='mb-4'>
-            KabadPe has created this privacy policy in order to demonstrate our firm
-commitment to your privacy. The following discloses our information gathering
-and dissemination practices relating to KabadPes websites (“Sites”), mobile
-applications, the delivery of our services, and other interactions with you. Please
-note that when you access any external links from KabadPes Sites (including any
-KabadPe subsidiary Sites), such external websites may have different privacy
-policies from the Sites and ClimStripe Shift Pvt. Ltd. is not responsible for the
-privacy practices of such external websites. We encourage you to read all privacy
-policies posted on the web sites that you visit.
+            <p className="mb-4">
+              KabadPe has created this privacy policy in order to demonstrate
+              our firm commitment to your privacy. The following discloses our
+              information gathering and dissemination practices relating to
+              KabadPes websites (“Sites”), mobile applications, the delivery of
+              our services, and other interactions with you. Please note that
+              when you access any external links from KabadPes Sites (including
+              any KabadPe subsidiary Sites), such external websites may have
+              different privacy policies from the Sites and ClimStripe Shift
+              Pvt. Ltd. is not responsible for the privacy practices of such
+              external websites. We encourage you to read all privacy policies
+              posted on the web sites that you visit.
             </p>
-            <p className='mb-4'>
-            This page is used to inform website visitors regarding our policies with the
-collection, use, and disclosure of Personal Information if anyone decided to use
-our Service, the Website Name website.
+            <p className="mb-4">
+              This page is used to inform website visitors regarding our
+              policies with the collection, use, and disclosure of Personal
+              Information if anyone decided to use our Service, the Website Name
+              website.
             </p>
-            <p className='mb-4'>
-            If you choose to use our Service, then you agree to the collection and use of
-information in relation with this policy. The Personal Information that we collect
-are used for providing and improving the Service. We will not use or share your
-information with anyone except as described in this Privacy Policy.
+            <p className="mb-4">
+              If you choose to use our Service, then you agree to the collection
+              and use of information in relation with this policy. The Personal
+              Information that we collect are used for providing and improving
+              the Service. We will not use or share your information with anyone
+              except as described in this Privacy Policy.
             </p>
-            <p className='mb-4'>
-            The terms used in this Privacy Policy have the same meanings as in our Terms
-and Conditions, which is accessible at Website URL, unless otherwise defined in
-this Privacy Policy.
+            <p className="mb-4">
+              The terms used in this Privacy Policy have the same meanings as in
+              our Terms and Conditions, which is accessible at Website URL,
+              unless otherwise defined in this Privacy Policy.
             </p>
           </div>
         </div>
       </section>
 
-
       <section className="serv-comp-one">
         <div className="comon-container-2">
-        <div className="serv-heading">
+          <div className="serv-heading">
             <h3>INFORMATION COLLECTION AND USE</h3>
-          
           </div>
           <div className="full-text ">
             <p>
-            For a better experience while using our Service, we may require you to provide us with
-certain personally identifiable information, including but not limited to your name,
-phone number, and postal address. The information that we collect will be used to
-contact or identify you.
+              For a better experience while using our Service, we may require
+              you to provide us with certain personally identifiable
+              information, including but not limited to your name, phone number,
+              and postal address. The information that we collect will be used
+              to contact or identify you.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>LOG DATA</h3>
+            <h3 style={{ paddingInline: "2rem" }}>LOG DATA</h3>
             <p>
-            We want to inform you that whenever you visit our Service, we collect information that
-your browser sends to us that is called Log Data. This Log Data may include
-information such as your computer&#39;s Internet Protocol (“IP”) address, browser version,
-pages of our Service that you visit, the time and date of your visit, the time spent on
-those pages, and other statistics.
+              We want to inform you that whenever you visit our Service, we
+              collect information that your browser sends to us that is called
+              Log Data. This Log Data may include information such as your
+              computer&#39;s Internet Protocol (“IP”) address, browser version,
+              pages of our Service that you visit, the time and date of your
+              visit, the time spent on those pages, and other statistics.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>COOKIES</h3>
+            <h3 style={{ paddingInline: "2rem" }}>COOKIES</h3>
             <p>
-            Cookies are files with small amount of data that is commonly used an anonymous
-unique identifier. These are sent to your browser from the website that you visit
-and are stored on your computer&#39;s hard drive. <br/>
-Our website uses these “cookies” to collection information and to improve our
-Service. You have the option to either accept or refuse these cookies, and know
-when a cookie is being sent to your computer. If you choose to refuse our cookies,
-you may not be able to use some portions of our Service.
+              Cookies are files with small amount of data that is commonly used
+              an anonymous unique identifier. These are sent to your browser
+              from the website that you visit and are stored on your
+              computer&#39;s hard drive. <br />
+              Our website uses these “cookies” to collection information and to
+              improve our Service. You have the option to either accept or
+              refuse these cookies, and know when a cookie is being sent to your
+              computer. If you choose to refuse our cookies, you may not be able
+              to use some portions of our Service.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>SERVICVE PROVIDERS</h3>
+            <h3 style={{ paddingInline: "2rem" }}>SERVICVE PROVIDERS</h3>
             <p>
-            We may employ third-party companies and individuals due to the following reasons: •
-To facilitate our Service; • To provide the Service on our behalf; • To perform Service-
-related services; or • To assist us in analysing how our Service is used.
-We want to inform our Service users that these third parties have access to your
-Personal Information. The reason is to perform the tasks assigned to them on our
-behalf. However, they are obligated not to disclose or use the information for any
-other purpose.
+              We may employ third-party companies and individuals due to the
+              following reasons: • To facilitate our Service; • To provide the
+              Service on our behalf; • To perform Service- related services; or
+              • To assist us in analysing how our Service is used. We want to
+              inform our Service users that these third parties have access to
+              your Personal Information. The reason is to perform the tasks
+              assigned to them on our behalf. However, they are obligated not to
+              disclose or use the information for any other purpose.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>SECURITY</h3>
+            <h3 style={{ paddingInline: "2rem" }}>SECURITY</h3>
             <p>
-            We value your trust in providing us your Personal Information, thus we are striving to
-use commercially acceptable means of protecting it. But remember that no method of
-transmission over the internet, or method of electronic storage is 100% secure and
-reliable, and we cannot guarantee its absolute security.
+              We value your trust in providing us your Personal Information,
+              thus we are striving to use commercially acceptable means of
+              protecting it. But remember that no method of transmission over
+              the internet, or method of electronic storage is 100% secure and
+              reliable, and we cannot guarantee its absolute security.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>LINKS TO OTHER SITES</h3>
+            <h3 style={{ paddingInline: "2rem" }}>LINKS TO OTHER SITES</h3>
             <p>
-            Our Service may contain links to other sites. If you click on a third-party link, you will be
-directed to that site. Note that these external sites are not operated by us.
-Therefore, we strongly advise you to review the Privacy Policy of these websites. We
-have no control over, and assume no responsibility for the content, privacy policies, or
-practices of any third-party sites or services.
-            </p>
-          </div>
-
-
-          <div className="full-text ">
-            <h5 style={{paddingInline : "2rem"}}>Children&#39;s Privacy</h5>
-            <p>
-            Our Services do not address anyone under the age of 13. We do not knowingly collect
-personal identifiable information from children under 13. In the case we discover that
-a child under 13 has provided us with personal information, we immediately delete this
-from our servers. If you are a parent or guardian and you are aware that your child has
-provided us with personal information, please contact us so that we will be able to do
-necessary actions.
+              Our Service may contain links to other sites. If you click on a
+              third-party link, you will be directed to that site. Note that
+              these external sites are not operated by us. Therefore, we
+              strongly advise you to review the Privacy Policy of these
+              websites. We have no control over, and assume no responsibility
+              for the content, privacy policies, or practices of any third-party
+              sites or services.
             </p>
           </div>
 
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>CHANGES TO THIS PRIVACY POLICY</h3>
+            <h5 style={{ paddingInline: "2rem" }}>Children&#39;s Privacy</h5>
             <p>
-            We may update our Privacy Policy from time to time. Thus, we advise you to review this
-page periodically for any changes. We will notify you of any changes by posting the new
-Privacy Policy on this page. These changes are effective immediately, after they are
-posted on this page.
+              Our Services do not address anyone under the age of 13. We do not
+              knowingly collect personal identifiable information from children
+              under 13. In the case we discover that a child under 13 has
+              provided us with personal information, we immediately delete this
+              from our servers. If you are a parent or guardian and you are
+              aware that your child has provided us with personal information,
+              please contact us so that we will be able to do necessary actions.
+            </p>
+          </div>
+
+          <div className="full-text ">
+            <h3 style={{ paddingInline: "2rem" }}>
+              CHANGES TO THIS PRIVACY POLICY
+            </h3>
+            <p>
+              We may update our Privacy Policy from time to time. Thus, we
+              advise you to review this page periodically for any changes. We
+              will notify you of any changes by posting the new Privacy Policy
+              on this page. These changes are effective immediately, after they
+              are posted on this page.
             </p>
           </div>
           <div className="full-text ">
-            <h3 style={{paddingInline : "2rem"}}>CONTACT US PRIVACY POLICY</h3>
+            <h3 style={{ paddingInline: "2rem" }}>CONTACT US PRIVACY POLICY</h3>
             <p>
-            If you have any questions or suggestions about our Privacy Policy, do not hesitate to
-contact us.
+              If you have any questions or suggestions about our Privacy Policy,
+              do not hesitate to contact us.
             </p>
           </div>
         </div>
       </section>
 
       <MainFooter />
-
     </>
-  )
-}
+  );
+};
 
-export default Privacypolicies
+export default Privacypolicies;

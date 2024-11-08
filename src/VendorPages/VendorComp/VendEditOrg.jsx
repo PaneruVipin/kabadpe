a4bcd95e1be9cef96b63870107ecb93f0a33613f
PaneruVipin <paneruvipinbusiness@gmail.com>
2024-11-08 14:44:57 +0530

    update vendor profile

--- src/VendorPages/VendorComp/VendEditOrg.jsx
+++ src/VendorPages/VendorComp/VendEditOrg.jsx
@@ -1,104 +1,273 @@
-import React, { useState } from 'react'
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { vendorOrgainazationUpdate } from "../../apis/vendor/vendor";
+import { toast } from "react-toastify";
 
-const VendEditOrg = ({onclickClose}) => {
-    const [switchBtn , setSwitchBtn] = useState(false);
-    const [tabActive , setTabActive] = useState('basic');
-
-  
-    
+const VendEditOrg = ({ onclickClose }) => {
+  const [switchBtn, setSwitchBtn] = useState(false);
+  const [tabActive, setTabActive] = useState("basic");
+  const { userInfo } = useSelector((s) => s?.user);
+  const handleSubmit = async (data) => {
+    const res = await vendorOrgainazationUpdate(data);
+    if (res?.error) {
+      toast.error(res?.message);
+      return;
+    }
+    toast.success(res);
+  };
   return (
     <>
-    <section className="add-prod-comp" onClick={onclickClose}>
-        
-        <div className="add-prod-main-bx add-prod-main-bx42" onClick={(e) => e.stopPropagation()}>
+      <section className="add-prod-comp" onClick={onclickClose}>
+        <div
+          className="add-prod-main-bx add-prod-main-bx42"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="top-add-prod-flex-bx">
+            <div className="left-add-prod-title-bx">
+              <h6>Edit Organization </h6>
+            </div>
 
-            <div className="top-add-prod-flex-bx">
+            <div className="right-lang-sel-bx-flex">
+              <div onClick={onclickClose} className="add-prod-close-btn">
+                <ion-icon name="close-outline"></ion-icon>
+              </div>
+            </div>
+          </div>
 
-                <div className="left-add-prod-title-bx">
-                    <h6>Edit Organization </h6>
-                </div>
-
-                <div className="right-lang-sel-bx-flex">
-
-                    <div onClick={onclickClose} className="add-prod-close-btn">
-                    <ion-icon name="close-outline"></ion-icon>
+          <Formik
+            initialValues={{
+              storeName: userInfo?.storeName,
+              companyName: userInfo?.companyName,
+              contactNumber: userInfo?.contactNumber,
+              sellarLegalName: userInfo?.VendorDetail?.sellarLegalName,
+              gstNumber: userInfo?.VendorDetail?.gstNumber,
+              addressLine1: userInfo?.VendorAddress?.addressLine1,
+              addressLine2: userInfo?.VendorAddress?.addressLine2,
+              city: userInfo?.VendorAddress?.city,
+              state: userInfo?.VendorAddress?.state,
+              pincode: userInfo?.VendorAddress?.pincode,
+              organizationEmail: userInfo?.organizationEmail,
+              aboutBrand: userInfo?.aboutBrand,
+              websiteUrl: userInfo?.websiteUrl,
+            }}
+            onSubmit={handleSubmit}
+          >
+            {({
+              handleBlur,
+              handleChange,
+              values,
+              errors,
+              touched,
+              ...rest
+            }) => {
+              return (
+                <Form className="add-prod-form-main">
+                  <div className="add-product-form-bx">
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Seller Legal Name</span>
+                      <input
+                        type="text"
+                        name="sellarLegalName"
+                        id="name"
+                        placeholder="Enter name"
+                        autoComplete="off"
+                        required
+                        value={values?.sellarLegalName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
                     </div>
-                    
-                </div>
-                
-            </div>
 
+                    <div className="ord-filt-bx add-prod-inpt-bx mt-5">
+                      <span>Company Name</span>
+                      <input
+                        type="text"
+                        name="companyName"
+                        id="compname"
+                        placeholder=""
+                        autoComplete="off"
+                        required
+                        value={values?.companyName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
 
-         
+                    <div className="ord-filt-bx add-prod-inpt-bx mt-5">
+                      <span>Store Name</span>
+                      <input
+                        type="text"
+                        name="storeName"
+                        id="compname"
+                        placeholder=""
+                        autoComplete="off"
+                        required
+                        value={values?.storeName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
 
-        <form action="" className="add-prod-form-main">
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>GST Number</span>
+                      <input
+                        type="text"
+                        name="gstNumber"
+                        id="gstnumber"
+                        placeholder="Enter GST number"
+                        autoComplete="off"
+                        value={values?.gstNumber}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
 
-            <div className="add-product-form-bx">
+                    <div className="ord-filt-bx add-prod-inpt-bx  add-prod-desc-bx">
+                      <span>Address Line1</span>
+                      <textarea
+                        name="addressLine1"
+                        id="address"
+                        cols="30"
+                        rows="2"
+                        placeholder="Enter address"
+                        autoComplete="off"
+                        required
+                        value={values?.addressLine1}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      ></textarea>
+                    </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx  add-prod-desc-bx">
+                      <span>Address Line2</span>
+                      <textarea
+                        name="addressLine2"
+                        id="address"
+                        cols="30"
+                        rows="2"
+                        placeholder="Enter address"
+                        autoComplete="off"
+                        value={values?.addressLine2}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      ></textarea>
+                    </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Post Code</span>
+                      <div>
+                        <input
+                          type="text"
+                          name="pincode"
+                          id="postcode"
+                          placeholder="Enter Post Code"
+                          autoComplete="off"
+                          required
+                          value={values?.pincode}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                      </div>
+                    </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>City</span>
+                      <div>
+                        <input
+                          type="text"
+                          name="city"
+                          id="postcode"
+                          placeholder="Enter Post Code"
+                          autoComplete="off"
+                          required
+                          value={values?.city}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                      </div>
+                    </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>State</span>
+                      <div>
+                        <input
+                          type="text"
+                          name="state"
+                          id="postcode"
+                          placeholder="Enter Post Code"
+                          autoComplete="off"
+                          required
+                          value={values?.state}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                      </div>
+                    </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Contact</span>
+                      <div>
+                        <input
+                          type="text"
+                          name="contactNumber"
+                          id="contact"
+                          placeholder="Enter number"
+                          autoComplete="off"
+                          value={values?.contactNumber}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                      </div>
+                    </div>
 
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Name</span>
-                <input type="text" name='name' id='name' placeholder='Enter name' autoComplete='off' required  />
-            </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Organization Email</span>
+                      <input
+                        type="email"
+                        name="organizationEmail"
+                        id="email"
+                        placeholder="example@gmail.com"
+                        autoComplete="off"
+                        value={values?.organizationEmail}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
 
-            
-            <div className="ord-filt-bx add-prod-inpt-bx mt-5">
-            <span>Company Name</span>
-                <input type="text" name='compname' id='compname' placeholder='HtmlLover Itd' autoComplete='off' required  />
-            </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Web site</span>
+                      <input
+                        type="text"
+                        name="websiteUrl"
+                        id="url"
+                        placeholder="Paste Web URL"
+                        autoComplete="off"
+                        value={values?.websiteUrl}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
+                  </div>
 
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>GST Number</span>
-                <input type="text" name='gstnumber' id='gstnumber' placeholder='Enter GST number' autoComplete='off' required  />
-            </div>
-
-            <div className="ord-filt-bx add-prod-inpt-bx  add-prod-desc-bx">
-            <span>Address</span>
-            <textarea name="address" id="address" cols="30" rows="3" placeholder='Enter address' autoComplete='off' required></textarea>
-            </div>
-
-            
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Post Code</span>
-            <div >
-                <input type="text" name='postcode' id='postcode' placeholder='Enter Post Code' autoComplete='off' required  />
-                </div>
-            </div>
-
-               
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Contact</span>
-            <div >
-                <input type="text" name='contact' id='contact' placeholder='Enter number' autoComplete='off' required  />
-                </div>
-            </div>
-
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Email</span>
-                <input type="email" name='email' id='email' placeholder='example@gmail.com' autoComplete='off' required  />
-            </div>
-
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Web site</span>
-                <input type="text" name='url' id='url' placeholder='Paste Web URL' autoComplete='off' required  />
-            </div>
-
-                
-            </div>
-            
-
-            
-            </form>
-            
-            <div className="prod-add-can-flex-btn prod-add-can-flex-btn21">
-                <button className='prod-add-del-btn upld-can-prod'>Cancel</button>
-                <button className='prod-add-del-btn upld-add-prod'>Add Product</button>
-            </div>
+                  <div className="prod-add-can-flex-btn prod-add-can-flex-btn21">
+                    <button
+                      type="button"
+                      className="prod-add-del-btn upld-can-prod"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      style={{ width: "300px" }}
+                      className="prod-add-del-btn upld-add-prod"
+                    >
+                      Update Organization
+                    </button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
-        
-    </section>
-      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default VendEditOrg
+export default VendEditOrg;
--- src/VendorPages/VendorComp/VendEditProf.jsx
+++ src/VendorPages/VendorComp/VendEditProf.jsx
@@ -1,112 +1,192 @@
-import React, { useState } from 'react'
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { vendorProfileUpdate } from "../../apis/vendor/vendor";
+import { toast } from "react-toastify";
 
-const VendEditProf = ({onclickClose}) => {
-    const [switchBtn , setSwitchBtn] = useState(false);
-    const [tabActive , setTabActive] = useState('basic');
-    const [images , setImages] = useState();
+const VendEditProf = ({ onclickClose }) => {
+  const [switchBtn, setSwitchBtn] = useState(false);
+  const [tabActive, setTabActive] = useState("basic");
+  const [images, setImages] = useState();
+  const { userInfo } = useSelector((s) => s?.user);
+  console.log("userInfouserInfouserInfouserInfo", userInfo);
+  const handleImageChange = (e) => {
+    const selectedImage = Array.from(e.target.files);
+    const imageUrl = selectedImage.map((image) => URL.createObjectURL(image));
+    setImages(imageUrl);
+  };
 
-    const handleImageChange = (e) => {
-
-        const selectedImage =  Array.from(e.target.files);
-        const imageUrl =  selectedImage.map(image => URL.createObjectURL(image));
-        setImages(imageUrl);
+  const handleDelete = () => {
+    setImages();
+  };
+  const handleSubmit = async (data) => {
+    const res = await vendorProfileUpdate(data);
+    if (res?.error) {
+      toast.error(res?.message);
+      return;
     }
-
-    const handleDelete = () => {
-        setImages() 
-        
-    }
-    
+    toast.success(res);
+  };
   return (
     <>
-    <section className="add-prod-comp" onClick={onclickClose}>
-        
-        <div className="add-prod-main-bx add-prod-main-bx42" onClick={(e) => e.stopPropagation()}>
+      <section className="add-prod-comp" onClick={onclickClose}>
+        <div
+          className="add-prod-main-bx add-prod-main-bx42"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="top-add-prod-flex-bx">
+            <div className="left-add-prod-title-bx">
+              <h6>Edit Profile </h6>
+            </div>
 
-            <div className="top-add-prod-flex-bx">
+            <div className="right-lang-sel-bx-flex">
+              <div onClick={onclickClose} className="add-prod-close-btn">
+                <ion-icon name="close-outline"></ion-icon>
+              </div>
+            </div>
+          </div>
 
-                <div className="left-add-prod-title-bx">
-                    <h6>Edit Profile </h6>
-                </div>
-
-                <div className="right-lang-sel-bx-flex">
-
-
-                    <div onClick={onclickClose} className="add-prod-close-btn">
-                    <ion-icon name="close-outline"></ion-icon>
+          <Formik
+            initialValues={{
+              fullname: userInfo?.fullname,
+              email: userInfo?.email,
+              phoneNumber: userInfo?.phoneNumber,
+              profileImage: userInfo?.profileImage,
+            }}
+            onSubmit={handleSubmit}
+          >
+            {({
+              handleBlur,
+              handleChange,
+              values,
+              errors,
+              touched,
+              ...rest
+            }) => {
+              return (
+                <Form  className="add-prod-form-main">
+                  <div className="add-product-form-bx">
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Name</span>
+                      <input
+                        type="text"
+                        name="fullname"
+                        id="profilename"
+                        placeholder="Enter Profile Name"
+                        autoComplete="off"
+                        required
+                        value={values?.fullname}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
                     </div>
-                    
-                </div>
-                
-            </div>
 
-
-        <form action="" className="add-prod-form-main">
-
-            <div className="add-product-form-bx">
-
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Name</span>
-                <input type="text" name='profilename' id='profilename' placeholder='Enter Profile Name' autoComplete='off' required  />
-            </div>
-
-          
-
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Profile Picture</span>
-                
-                <div className="right-prod-img-add-bx">
-            <div className="right-prod-upload-img-bx-main">
-
-                <input type="file" accept='image/*' multiple id='imageUpload' onChange={handleImageChange} />
-
-                <label htmlFor="imageUpload" >
-                <ion-icon name="cloud-upload-outline"></ion-icon>
-                <p>Drag your images here</p>
-                <span> (Only *jpeg, *webp and * png images will be accepted) </span>
-                </label>
-                
-            </div>
-
-           { images ? <div className="image-add-prof">
-
-                        <div className="upld-prod-img-bx upld-prod-img-bx21" >
-                            <img src={images} alt="" />
-                            <button onClick={() => handleDelete()} className="delete-prod-img-bx">
-                            <ion-icon name="close-outline"></ion-icon>
-                            </button> 
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <div>
+                        <span>Profile Picture</span>{" "}
+                        <div className="image-add-prof">
+                          <div className="upld-prod-img-bx upld-prod-img-bx21">
+                            <img
+                              src={
+                                !values?.profileImage ||
+                                typeof values?.profileImage == "string"
+                                  ? values?.profileImage ||
+                                    "/images/no_profile.webp"
+                                  : URL.createObjectURL(values?.profileImage)
+                              }
+                              alt=""
+                            />
+                            <button
+                              type="button"
+                              onClick={() => {
+                                handleChange({
+                                  target: { name: "profileImage", value: "" },
+                                });
+                              }}
+                              className="delete-prod-img-bx"
+                            >
+                              <ion-icon name="close-outline"></ion-icon>
+                            </button>
+                          </div>
                         </div>
-                    
-            </div> : null}
-            
-            </div>
-                </div>
+                      </div>
 
-                
-            <div className="ord-filt-bx add-prod-inpt-bx mt-5">
-            <span>Email</span>
-                <input type="email" name='profileemail' id='profileemail' placeholder='admin@gmail.com' autoComplete='off' required  />
-            </div>
+                      <div className="right-prod-img-add-bx">
+                        <div className="right-prod-upload-img-bx-main">
+                          <input
+                            type="file"
+                            accept="image/*"
+                            multiple
+                            id="imageUpload"
+                            onChange={(e) => {
+                              const file = e.target.files[0];
+                              if (file) {
+                                handleChange({
+                                  target: { name: "profileImage", value: file },
+                                });
+                              }
+                            }}
+                          />
 
-            <div className="ord-filt-bx add-prod-inpt-bx">
-            <span>Contact Number</span>
-                <input type="number" name='contnumber' id='contnumber' placeholder='Enter Phone number' autoComplete='off' required  />
-            </div>
+                          <label htmlFor="imageUpload">
+                            <ion-icon name="cloud-upload-outline"></ion-icon>
+                            <p>Update your images here</p>
+                            <span>
+                              {" "}
+                              (Only *jpeg, *webp and * png images will be
+                              accepted){" "}
+                            </span>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
 
-         
-                
-            </div>
-            
-            </form>
-            <div className="prod-add-can-flex-btn prod-add-can-flex-btn21">
-                <button className='prod-add-del-btn upld-add-prod'>Update Profile</button>
-            </div>
+                    <div className="ord-filt-bx add-prod-inpt-bx mt-5">
+                      <span>Email</span>
+                      <input
+                        type="email"
+                        name="email"
+                        id="profileemail"
+                        placeholder="admin@gmail.com"
+                        autoComplete="off"
+                        required
+                        value={values?.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
+
+                    <div className="ord-filt-bx add-prod-inpt-bx">
+                      <span>Contact Number</span>
+                      <input
+                        type="number"
+                        name="phoneNumber"
+                        id="contnumber"
+                        placeholder="Enter Phone number"
+                        autoComplete="off"
+                        required
+                        value={values?.phoneNumber}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                    </div>
+                  </div>
+                  <div className="prod-add-can-flex-btn prod-add-can-flex-btn21">
+                    <button
+                      type="submit"
+                      className="prod-add-del-btn upld-add-prod"
+                    >
+                      Update Profile
+                    </button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
-        
-    </section>
-      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default VendEditProf
+export default VendEditProf;

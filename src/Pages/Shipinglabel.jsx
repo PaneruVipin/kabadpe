032465895543b9551801570843754a56f7e44c5b
PaneruVipin <paneruvipinbusiness@gmail.com>
2024-12-06 16:07:54 +0530

    miner

--- src/Pages/Shipinglabel.jsx
+++ src/Pages/Shipinglabel.jsx
@@ -6,11 +6,11 @@ import { DateTime } from "luxon";
 import { numberToWords } from "../lib/number";
 import generatePDF from "react-to-pdf";
 
-const Invoice = ({ orderId }, ref) => {
+const Shiping = ({ orderId }, ref) => {
   const targetRef = useRef();
   const { id } = useParams();
   const { data: order, refetch } = useQuery({
-    queryKey: ["orderdetail272967876543"],
+    queryKey: ["orderdetail27293563637"],
     queryFn: () => userOrderDetailfetch({ id: orderId || id }),
   });
   console.log("ref & targetRef", ref, targetRef, order, orderId);
@@ -40,7 +40,7 @@ const Invoice = ({ orderId }, ref) => {
             <button
               onClick={() =>
                 generatePDF(targetRef, {
-                  filename: `TGSS-invoice-${order?.id}`,
+                  filename: `TGSS-shipping-${order?.id}`,
                 })
               }
               style={{
@@ -79,32 +79,32 @@ const Invoice = ({ orderId }, ref) => {
               </div>
 
               <div className="invoice-top-text-flex">
-                <h6>Tax Invoice/Bill of Supply/Cash Memo</h6>
-                <p>(Original for Recipient)</p>
+                {/* <h6>Tax Invoice/Bill of Supply/Cash Memo</h6>
+                <p>(Original for Recipient)</p> */}
               </div>
             </div>
 
             <div className="invoice-two-grid-bx">
               <div className="invoice-info-bx">
-                <h6>Sold By :</h6>
+                <h6>From :</h6>
                 <p>
                   {vendor?.companyName}
-                  <br />
+                  {/* <br />
                   {`${vendorAdddress?.addressLine1}, ${
                     vendorAdddress?.addressLine2
                       ? vendorAdddress?.addressLine2 + ","
                       : ""
                   } ${vendorAdddress?.city}, ${vendorAdddress?.state}`}
-                  <br /> {`${vendorAdddress?.pincode} IN`}
+                  <br /> {`${vendorAdddress?.pincode} IN`} */}
                 </p>
               </div>
 
               <div className="invoice-info-bx invoice-info-bx2">
-                <h6>Billing Address :</h6>
+                {/* <h6>Billing Address :</h6>
                 <p>{vendor?.fullname} </p>
                 <p>{order?.billingAddress || order?.shippingAddress}</p>
 
-                <p>IN</p>
+                <p>IN</p> */}
                 {/* <h6>
                 State/UT Code: <span>08</span>{" "}
               </h6> */}
@@ -114,13 +114,13 @@ const Invoice = ({ orderId }, ref) => {
             <div className="invoice-two-grid-bx">
               <div className="invoice-relt">
                 <div className="invoice-info-bx">
-                  <h6>
+                  {/* <h6>
                     PAN No: <span> {vendorDetail?.panNumber}</span>
                   </h6>
                   <h6>
                     GST Registration No:{" "}
                     <span> {vendorDetail?.gstNumber} </span>
-                  </h6>
+                  </h6> */}
                 </div>
 
                 <div className="invoice-info-bx invoice-info-bx3">
@@ -150,7 +150,7 @@ const Invoice = ({ orderId }, ref) => {
                 {/* <h6>
                 State/UT Code: <span>08</span>{" "}
               </h6> */}
-                <h6>
+                {/* <h6>
                   Place of supply: <span>{vendorAdddress?.state}</span>{" "}
                 </h6>
                 <h6>
@@ -158,11 +158,11 @@ const Invoice = ({ orderId }, ref) => {
                 </h6>
                 <h6>
                   Invoice Number: <span>{order?.id}</span>{" "}
-                </h6>
+                </h6> */}
                 {/* <h6>
                 Invoice Details: <span> RJ-JPX2-137392831-2425</span>{" "}
               </h6> */}
-                <h6>
+                {/* <h6>
                   Invoice Date:{" "}
                   <span>
                     {DateTime.fromISO(new Date().toISOString(), {
@@ -171,155 +171,21 @@ const Invoice = ({ orderId }, ref) => {
                       .setZone("Asia/Kolkata")
                       .toFormat("dd.LL.yyyy")}
                   </span>{" "}
-                </h6>
+                </h6> */}
               </div>
             </div>
 
-            <div className="invoice-table">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Sl. No</th>
-                    <th>Description</th>
-                    <th>Unit Price</th>
-                    <th>Qty</th>
-                    <th>Net Amount</th>
-                    <th>Tax Rate</th>
-                    <th>Tax Type</th>
-                    <th>Tax Amount</th>
-                    <th>Donation</th>
-                    <th>Total Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {OrderItems?.map(
-                    ({
-                      id,
-                      productName,
-                      quantity,
-                      unitPrice,
-                      totalPrice,
-                      netPrice,
-                      gst,
-                    }) => {
-                      const gstAmount = gst ? (netPrice * gst) / 100 : 0;
-                      return (
-                        <tr key={id}>
-                          <td>
-                            <span>1</span>
-                          </td>
-                          <td>
-                            <span>{productName}</span>
-                          </td>
-
-                          <td>
-                            <span>₹{unitPrice}</span>
-                          </td>
-                          <td>
-                            <span>{quantity}</span>
-                          </td>
-                          <td>
-                            <span>
-                              ₹
-                              {netPrice ||
-                                (unitPrice * quantity || 0)?.toFixed(2)}
-                            </span>
-                          </td>
-                          <td>
-                            <span>{gst / 2}%</span>
-                            <br />
-                            <span>{gst / 2}%</span>
-                          </td>
-                          <td>
-                            <span>CGST</span>
-                            <br />
-                            <span>SGST</span>
-                          </td>
-                          <td>
-                            <span> ₹{(gstAmount / 2)?.toFixed(2)}</span>
-                            <br />
-                            <span> ₹{(gstAmount / 2).toFixed(2)}</span>
-                          </td>
-                          <td>
-                            <span></span>
-                          </td>
-                          <td>
-                            <span>₹{totalPrice}</span>
-                          </td>
-                        </tr>
-                      );
-                    }
-                  )}
-                  <tr>
-                    <td></td>
-                    <td>
-                      <span></span>
-                    </td>
-
-                    <td>
-                      {" "}
-                      <span> ₹{total?.unitPrice?.toFixed(2)}</span>
-                    </td>
-                    <td></td>
-                    <td>
-                      {" "}
-                      <span> ₹{total?.netPrice?.toFixed(2)}</span>
-                    </td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                      <span> ₹{total?.gst?.toFixed(2)}</span>
-                    </td>
-                    <td>
-                      <span>₹{order?.donation}</span>
-                    </td>
-                    <td>
-                      <span>₹{total?.total?.toFixed(2)}</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
-            <div className="amount-words-bx">
-              <h6>Amount in Words:</h6>
-              <p>{numberToWords(Math.floor(total?.total))}</p>
-            </div>
-
-            <div className="sign-bx amount-words-bx with_border">
-              <h5>For {vendor?.companyName}:</h5>
-
-              {/* <div className="sign">
-                <img src="/images/my-sign.jpg" alt="" />
-              </div> */}
-
-              <h5>{vendor?.fullname}</h5>
-            </div>
-            <p className="outer-bx-text">
-              Whether tax is payable under reverse charge - No
-            </p>
-
-            <div className="payment-trans-grid-bx">
+            <div className="payment-trans-grid-bx2">
               <div className="payment-trnas-bx">
-                <h6>Payment Transaction ID: </h6>
-                <p>{order?.TnxRecord?.txnId}</p>
-              </div>
-              <div className="payment-trnas-bx">
-                <h6>Date & Time: 1 </h6>
-                <p>
-                  {DateTime.now()
-                    .setZone("Asia/Kolkata")
-                    .toFormat("dd/LL/yyyy, HH:mm:ss 'hrs'")}
-                </p>
-              </div>
-              <div className="payment-trnas-bx">
-                <h6>Invoice Value: </h6>
-                <p>{total?.total}</p>
+                <h6>Amount: </h6>
+                <p> ₹{order?.orderAmount}</p>
               </div>
 
               <div className="payment-trnas-bx">
                 <h6>Mode of Payment: </h6>
-                <p>{`${order?.paymentMethod} (${order?.paymentStatus})`}</p>
+                <p>{`${
+                  order?.paymentMethod == "cash" ? "COD" : "Pre-Paid"
+                }`}</p>
               </div>
             </div>
             <p className="bottom-line">
@@ -338,4 +204,4 @@ const Invoice = ({ orderId }, ref) => {
   );
 };
 
-export default Invoice;
+export default Shiping;
